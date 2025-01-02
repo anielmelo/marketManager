95f94288a7cf6013724db252df95622005b5bcc4
anielmelo <anielcontato@gmail.com>
2025-01-02 15:48:26 -0300

    feat: add initial authentication microservice configuration

---
+++ authentication/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/authentication/AuthenticationMarketmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.esperanca.ads.marketmanager.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationMarketmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationMarketmanagerApplication.class, args);
+	}
+
+}
