c6980379a62fb21ccbdbb3454f901a6fe405dc4c
anielmelo <anielcontato@gmail.com>
2025-01-02 15:47:41 -0300

    feat: add initial gateway configuration

---
+++ gateway/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/gateway/GatewayMarketmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.esperanca.ads.marketmanager.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMarketmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMarketmanagerApplication.class, args);
+	}
+
+}
