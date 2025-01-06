c4780d6a4fa532bb2b3cf064f60526c8da24f357
anielmelo <anielcontato@gmail.com>
2025-01-06 16:42:10 -0300

    feat: add gateway initial configuration

---
+++ gateway/src/test/java/br/edu/ifpb/esperanca/ads/marketmanager/gateway/GatewayMarketmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.esperanca.ads.marketmanager.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMarketmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
