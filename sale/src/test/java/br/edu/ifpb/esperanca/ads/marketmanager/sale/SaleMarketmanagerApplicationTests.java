e26387bc3606e154dab40cfa58b4660b56d9876a
anielmelo <anielcontato@gmail.com>
2025-01-06 16:44:52 -0300

    feat: add initial configuration for the sales microservice

---
+++ sale/src/test/java/br/edu/ifpb/esperanca/ads/marketmanager/sale/SaleMarketmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.esperanca.ads.marketmanager.sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleMarketmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
