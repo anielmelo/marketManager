f097954ae7ce59f1a0eb2754793a54b5a555d877
dinizhugo <hugodiniztech01@gmail.com>
2025-02-01 23:00:24 -0300

    feat: Create mappers

---
+++ inventory/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/inventory/mappers/ProductMapper.java
@@ -0,0 +1,37 @@
+package br.edu.ifpb.esperanca.ads.marketmanager.inventory.mappers;
+
+import br.edu.ifpb.esperanca.ads.marketmanager.inventory.dtos.ProductRequestDTO;
+import br.edu.ifpb.esperanca.ads.marketmanager.inventory.dtos.ProductResponseDTO;
+import br.edu.ifpb.esperanca.ads.marketmanager.inventory.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+
+    public Product toEntity(ProductRequestDTO dto) {
+        Product product = new Product();
+
+        product.setName(dto.name());
+        product.setBrand(dto.brand());
+        product.setCost(dto.cost());
+        product.setAvailableQuantity(dto.availableQuantity());
+        product.setTotalQuantity(dto.totalQuantity());
+        product.setSupplier(dto.supplierId());
+        product.setReceiving(dto.receivingId());
+
+        return product;
+    }
+
+    public ProductResponseDTO toProductResponseDTO(Product product) {
+
+        return new ProductResponseDTO(
+                product.getName(),
+                product.getBrand(),
+                product.getCost(),
+                product.getAvailableQuantity(),
+                product.getTotalQuantity(),
+                product.getSupplier(),
+                product.getReceiving()
+        );
+    }
+}
---
+++ inventory/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/inventory/mappers/SupplierMapper.java
@@ -0,0 +1,22 @@
+package br.edu.ifpb.esperanca.ads.marketmanager.inventory.mappers;
+
+import br.edu.ifpb.esperanca.ads.marketmanager.inventory.dtos.SupplierRequestDTO;
+import br.edu.ifpb.esperanca.ads.marketmanager.inventory.dtos.SupplierResponseDTO;
+import br.edu.ifpb.esperanca.ads.marketmanager.inventory.model.Supplier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SupplierMapper {
+
+    public Supplier toEntity(SupplierRequestDTO dto) {
+        return new Supplier(null, dto.name(), dto.cnpj(), dto.address());
+    }
+
+    public SupplierResponseDTO toDTO(Supplier supplier) {
+        return new SupplierResponseDTO(
+                supplier.getId(),
+                supplier.getName(),
+                supplier.getCnpj(),
+                supplier.getAddress());
+    }
+}
