463856a385860b900cb91a5e94f8e05fb641275c
dinizhugo <hugodiniztech01@gmail.com>
2025-02-05 23:22:37 -0300

    Refactor: Modify methods

--- inventory/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/inventory/mappers/ProductMapper.java
+++ inventory/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/inventory/mappers/ProductMapper.java
@@ -1,37 +1,36 @@
 package br.edu.ifpb.esperanca.ads.marketmanager.inventory.mappers;
 
-import br.edu.ifpb.esperanca.ads.marketmanager.inventory.dtos.ProductRequestDTO;
-import br.edu.ifpb.esperanca.ads.marketmanager.inventory.dtos.ProductResponseDTO;
+import br.edu.ifpb.esperanca.ads.marketmanager.inventory.dtos.*;
 import br.edu.ifpb.esperanca.ads.marketmanager.inventory.models.Product;
+import br.edu.ifpb.esperanca.ads.marketmanager.inventory.models.Receiving;
+import br.edu.ifpb.esperanca.ads.marketmanager.inventory.models.Supplier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductMapper {
 
-    public Product toEntity(ProductRequestDTO dto) {
+    public Product toEntity(ProductRequestDTO dto, Supplier supplier, Receiving receiving) {
         Product product = new Product();
 
         product.setName(dto.name());
         product.setBrand(dto.brand());
         product.setCost(dto.cost());
         product.setAvailableQuantity(dto.availableQuantity());
-        product.setTotalQuantity(dto.totalQuantity());
-        product.setSupplier(dto.supplierId());
-        product.setReceiving(dto.receivingId());
+        product.setSupplier(supplier);
+        product.setReceiving(receiving);
 
         return product;
     }
 
     public ProductResponseDTO toProductResponseDTO(Product product) {
-
         return new ProductResponseDTO(
+                product.getId(),
                 product.getName(),
                 product.getBrand(),
                 product.getCost(),
                 product.getAvailableQuantity(),
-                product.getTotalQuantity(),
-                product.getSupplier(),
-                product.getReceiving()
+                new SupplierResponseDTO(product.getSupplier().getId(), product.getSupplier().getName(), product.getSupplier().getCnpj(), product.getSupplier().getAddress()),
+                new ReceivingResponseDTO(product.getReceiving().getId(), product.getReceiving().getDate())
         );
     }
 }
