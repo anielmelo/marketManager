8c9acdbc3f57ede6f1aacca6467500961df5296b
anielmelo <anielcontato@gmail.com>
2025-02-27 21:57:08 -0300

    feat: update dtos

--- inventory/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/inventory/dtos/ReplacementResponseDTO.java
+++ inventory/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/inventory/dtos/ReplacementResponseDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 public record ReplacementResponseDTO(
         Long id,
+        String idStockKeeper,
         LocalDateTime date,
         Double totalPurchaseValue,
         Integer quantityReceived) { }
