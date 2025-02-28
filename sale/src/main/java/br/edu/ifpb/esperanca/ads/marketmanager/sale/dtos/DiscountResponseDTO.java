af1985a03bbd9519f1757026285d77085a585d65
anielmelo <anielcontato@gmail.com>
2025-02-28 00:17:07 -0300

    feat: update dtos

--- sale/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/sale/dtos/DiscountResponseDTO.java
+++ sale/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/sale/dtos/DiscountResponseDTO.java
@@ -2,6 +2,7 @@ package br.edu.ifpb.esperanca.ads.marketmanager.sale.dtos;
 
 public record DiscountResponseDTO (
         Long id,
+        String idSaleKeeper,
         String code,
         String description,
         Double value,
