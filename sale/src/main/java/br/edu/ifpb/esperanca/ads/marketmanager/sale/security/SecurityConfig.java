c61e676359b14a12283fc6bb0d5e867060182c45
anielmelo <anielcontato@gmail.com>
2025-03-08 22:57:44 -0300

    refactor: remove unused security configuration
    remove csrf and session policy from security configuration.

--- sale/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/sale/security/SecurityConfig.java
+++ sale/src/main/java/br/edu/ifpb/esperanca/ads/marketmanager/sale/security/SecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,9 +18,6 @@ public class SecurityConfig {
 
                 // hasAuthority("SALE_KEEPER")
                 return http
-                                .csrf((AbstractHttpConfigurer::disable))
-                                .sessionManagement(session -> session
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                                 .authorizeHttpRequests(authorize -> authorize
                                                 .requestMatchers(HttpMethod.GET, "/sale/**").hasRole("SALE_KEEPER")
                                                 .requestMatchers(HttpMethod.POST, "/sale/**").hasRole("SALE_KEEPER")
